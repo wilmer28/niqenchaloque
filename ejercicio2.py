ee067ff4c03e714b47c866b6175c961505f2fb46
wilmer28 <57870232+wilmer28@users.noreply.github.com>
2019-11-17 12:42:24 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+import os
+#declaracion de variables
+curso,ejercicios_resueltos="",0.0
+
+#imput
+curso=str(os.sys.argv[1])
+ejercicios_resueltos=int(os.sys.argv[2])
+
+#outpout
+print("################################")
+print("total de ejercicios resueltos")
+print("################################")
+print("#curso                   :",curso)
+print("#ejercicios resueltos    :",ejercicios_resueltos)
+print("################################")
+
+#condicionales simples
+if(ejercicios_resueltos>100):
+    print("te ganaste dos punto extra")
