5314d24fb4a49d40e0c0124e2644e6ecc5d4326f
wilmer28 <57870232+wilmer28@users.noreply.github.com>
2019-11-17 12:45:19 -0500

    Create ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,17 @@
+import os
+#declaracion de variables
+numeros_de_tickets_de_happyland=0.0
+
+#imput
+numeros_de_tickets_de_happyland=int(os.sys.argv[1])
+
+#outpout
+print("################################")
+print("tickets de happyland")
+print("################################")
+print("#ticketsde happyland     :",numeros_de_tickets_de_happyland)
+print("################################")
+
+#condicionales simples
+if(numeros_de_tickets_de_happyland>200):
+    print("te ganaste un ticket mas")
