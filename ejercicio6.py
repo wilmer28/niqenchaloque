affc172b3abfd7f9d5a522fc6da38cc23e37e678
wilmer28 <57870232+wilmer28@users.noreply.github.com>
2019-11-17 12:45:52 -0500

    Create ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,19 @@
+import os
+#declaracion de variables
+cliente,precio_de_los_productos="",0.0
+
+#imput
+cliente=str(os.sys.argv[1])
+precio_de_los_productos=int(os.sys.argv[2])
+
+#outpout
+print("################################")
+print("descuento de los productos")
+print("################################")
+print("#cliente                   :",cliente)
+print("#precio de los productos   :",precio_de_los_productos)
+print("################################")
+
+#condicionales simples
+if(precio_de_los_productos>400):
+    print("tiene un descuento del 20%")
