dfc44806340469e46820e309345faad5a8ca045d
wilmer28 <57870232+wilmer28@users.noreply.github.com>
2019-11-17 12:47:57 -0500

    Create dobles.py

---
+++ dobles.py
@@ -0,0 +1,29 @@
+import os
+#declaracion de variables
+nombre,nota1,nota2,nota3="",0.0,0.0,0.0
+
+#imput
+nombre=str(os.sys.argv[1])
+nota1=int(os.sys.argv[2])
+nota2=int(os.sys.argv[3])
+nota3=int(os.sys.argv[4])
+
+#processing
+promedio=str((nota1+nota2+nota3)/3)
+
+#outpout
+print("################################")
+print("el promedio de notas")
+print("################################")
+print("#nombre               :",nombre)
+print("#nota1                :",nota1)
+print("#nota2                :",nota2)
+print("#nota3                :",nota3)
+print("#promedio             :",promedio)
+print("################################")
+
+#condicionales dobles
+if(promedio>10):
+    print("el alumno esta aprobado")
+else:
+    print("el alunmo esta desaprobado")
